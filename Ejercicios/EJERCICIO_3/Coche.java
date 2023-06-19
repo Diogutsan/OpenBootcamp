29a609b5c650c0a1a9c47b381f9ff04a84207e37
Diovanis Gutierrez <121168282+Diogutsan@users.noreply.github.com>
2023-06-19 15:47:43 -0500

    modificaciones por error

---
+++ Ejercicios/EJERCICIO_3/Coche.java
@@ -0,0 +1,9 @@
+public class Coche {
+
+    public int puertas = 0;
+
+    public void SumarPuerta() {
+        this.puertas++;
+    }
+
+}
--- Ejercicios/EJERCICIO_3/Main.java
+++ Ejercicios/EJERCICIO_3/Main.java
@@ -3,28 +3,15 @@
 //OpenBootcamp
 
 public class Main {
-	public static void main( String [ ] args) {
-		int a=6;
-		int b=2;
-		int c=8;
-		int suma = a+b+c;
+	public static void main(String[] args) {
+		int a = 6;
+		int b = 2;
+		int c = 8;
+		int suma = a + b + c;
 		System.out.print(suma);
-	}  
-	
-	public static void main (String [ ] args){
-	
-	coche miCoche =new coche();
-	miCoche.Sumarpuerta();
-	System.out.println(miCoche.puertas)
-	}
-}
 
-public class coche{
-        
-        public int puertas = 0;
-        
-        public void SumarPuerta(){
-            this.puertas++;
-        }
-        
+		Coche miCoche = new Coche();
+		miCoche.SumarPuerta();
+		System.out.print(miCoche.puertas);
+	}
 }
