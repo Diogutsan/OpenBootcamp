fa5f6090b4d37671cb73c7c574086df31de96c72
Diovanis Gutierrez <121168282+Diogutsan@users.noreply.github.com>
2023-06-19 16:10:28 -0500

    correccion

--- Ejercicios/EJERCICIO_4/Main.java
+++ Ejercicios/EJERCICIO_4/Main.java
@@ -21,20 +21,26 @@ public class Main {
     }
 
     public static void funcionIf(int numero) {
+        System.out.println("Esta es la funcion if");
         if (numero < 0) {
 
-            System.out.println(numero + "Este numero es negativo");
+            System.out.println(numero + " -Es negativo");
+
+        }
+        if (numero == 0) {
+
+            System.out.println(numero + " Es cero");
 
         } else {
 
-            System.out.println(numero + "Este numero es positivo");
+            System.out.println(numero + "Es positivo");
 
         }
 
     }
 
     public static void funcionWhile(int numero) {
-
+        System.out.println("Esta es la funcion While");
         while (numero < 3) {
 
             System.out.println(numero);
@@ -43,7 +49,7 @@ public class Main {
     }
 
     public static void funcionDo(int numero) {
-
+        System.out.println("Esta es la funcion Do-02-");
         do {
             System.out.println(numero);
             numero = numero += 1;
