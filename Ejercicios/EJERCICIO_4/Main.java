eb2478a7a32403ec1d6e7f42c3a65f077a88e35a
Diovanis Gutierrez <121168282+Diogutsan@users.noreply.github.com>
2023-06-19 15:56:23 -0500

    mas correcciones

--- Ejercicios/EJERCICIO_4/Main.java
+++ Ejercicios/EJERCICIO_4/Main.java
@@ -1,42 +1,53 @@
+
 //Ejercicios tema 4
 //introduccion a la programacion
 //OpenBootcamp
 import java.util.scanner;
+
 public class Main {
 
-	public static void main( String [ ] args) {
-        
+    public static void main(String[] args) {
+
         System.out.print("Ingrese un nuemro Negativo o Positivo: ");
         Scanner scanner = new Scanner(System.in);
         int numero = scanner.nextLine();
         scanner.close();
-        
+
         funcionIf();
         funcionWhile();
-        funcionDo();    
-    
-	}
-    public static void funcionIf(int numero)  {
-        if numero <0;
-            System.out.println(numero, 'Este numero es negativo');
-        else;
-            System.out.println(numero, 'Este numero es positivo');
+        funcionDo();
+
     }
+
+    public static void funcionIf(int numero) {
+        if (numero < 0) {
+
+            System.out.println(numero, "Este numero es negativo");
+
+        } else {
+
+            System.out.println(numero, "Este numero es positivo");
+
+        }
+
+    }
+
     public static void funcionWhile(int numero) {
-        
+
         while (numero < 3) {
-        
+
             System.out.println(numero);
             numero = numero += 1;
         }
     }
-    public static void funcionDo(int numero) { 
-  
-        do{
-		    System.out.println(numero);
-            numero = número += 1;
-  
-        }while (numeroWhile < 3); 
-  
+
+    public static void funcionDo(int numero) {
+
+        do {
+            System.out.println(numero);
+            numero = numero += 1;
+
+        } while (numero < 3);
+
     }
-}	
+}
